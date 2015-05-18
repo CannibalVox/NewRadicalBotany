361b041b667c191b7e51d22bb30f5245796ff447
CannibalVox <sdbaynham@gmail.com>
2015-05-18 08:06:52 -0500

    Initial checkin

---
+++ src/main/java/net/technicpack/newradicalbotany/NewRadicalBotany.java
@@ -0,0 +1,9 @@
+package net.technicpack.newradicalbotany;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = NewRadicalBotany.MODID, version = NewRadicalBotany.VERSION)
+public class NewRadicalBotany {
+    public static final String MODID = "newradicalbotany";
+    public static final String VERSION = "GRADLE:VERSION.GRADLE:BUILD";
+}
